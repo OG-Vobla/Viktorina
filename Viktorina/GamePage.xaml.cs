4f363b23f7613be3c061b3aacb57c9070bfaa9c0
zubr radel <gggg26705@gmail.com>
2023-01-19 11:00:24 +0300

    Сделал

--- Viktorina/GamePage.xaml.cs
+++ Viktorina/GamePage.xaml.cs
@@ -33,15 +33,49 @@ namespace Viktorina
 		int[] closeNums;
 		int newMargin;
 		int cnt;
+		int cnt1;
 		public GamePage()
 		{
 			InitializeComponent();
 			questionList = Question.FindAllInDb();
 			rnd = new Random();
+			cnt1 = 0;
 			labels = new List<System.Windows.Controls.Label>();
 			answerLabels = new List<System.Windows.Controls.Label>();
 			Buttons = new List<Button>();
 			newMargin = 0;
+			Buttons.Clear();
+			TakeRandomQuest();
+			SetAnswer();
+			closeNums = new int[question.Answer.Length];
+			Answer = new char[question.Answer.Length];
+			newMargin = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
+			cnt = 0;
+			SetKeyboard();
+		}
+		public void SetAllParam()
+		{
+			cnt1 = 0;
+
+			newMargin = 0;
+			foreach (var j in labels)
+			{
+				canvasmiddle.Children.Remove(j);
+			}
+			labels.Clear();
+			foreach (var j in answerLabels)
+			{
+				canvasmiddle.Children.Remove(j);
+			}
+			answerLabels.Clear();
+			foreach (var j in Buttons)
+			{
+				canvas.Children.Remove(j);
+			}
+			Buttons.Clear();
+			labels = new List<System.Windows.Controls.Label>();
+			answerLabels = new List<System.Windows.Controls.Label>();
+			Buttons = new List<Button>();
 			TakeRandomQuest();
 			SetAnswer();
 			closeNums = new int[question.Answer.Length];
@@ -53,16 +87,34 @@ namespace Viktorina
 		public void TakeRandomQuest()
 		{
 			int random = rnd.Next(0, questionList.Count);
+			if (questionList.Count == 0)
+			{
+				MessageBox.Show("Вопросы закончились");
+				this.NavigationService.GoBack();
+				return;
+			}
 			question = questionList[random];
 			Description.Text = question.Description;
-			questionList.Remove(question);
 		}
 		public void SetAnswer()
 		{
-			int margin = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
+			int margin;
+			int margin1 = 0 ;
+			if (question.Answer.Length > 20)
+			{
+				margin = Convert.ToInt32(canvasmiddle.Width / 2 - ((question.Answer.Length - (question.Answer.Length - 20)) * 36) / 2);
+				margin1 = Convert.ToInt32(canvasmiddle.Width / 2 - ((question.Answer.Length - (question.Answer.Length - 20)) * 36) / 2);
+
+			}
+			else
+			{
+				 margin = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
+
+				margin1 = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
+			}
 			foreach (var i in question.Answer)
 			{
-
+				
 				System.Windows.Controls.Label Buttonitem = new System.Windows.Controls.Label();
 				Buttonitem.Content = "";
 				Buttonitem.Height = 35;
@@ -71,11 +123,22 @@ namespace Viktorina
 				Thickness m = Buttonitem.Margin;
 				m.Left = margin;
 				margin += 36;
+				if (cnt1 >= 20)
+				{
+					if (cnt1 == 20)
+					{
+						m.Left = margin1;
+						margin = margin1 + 36;
+					}
+					m.Top = 37;
+				}
+				cnt1++;
 				Buttonitem.Margin = m;
 				Buttonitem.Foreground = new SolidColorBrush(Colors.White);
 				Buttonitem.Content = i;
 				Buttonitem.FontFamily = new FontFamily("Comic Sans MS");
 				Buttonitem.FontSize = 20;
+
 				labels.Add(Buttonitem);
 				canvasmiddle.Children.Add(Buttonitem);
 
@@ -89,44 +152,47 @@ namespace Viktorina
 				Answer[cnt] = Convert.ToChar(button.Content);
 				System.Windows.Controls.Label Buttonitem = new System.Windows.Controls.Label();
 				Buttonitem.Content = button.Content;
-				cnt++;
 				Buttonitem.Height = 35;
 				Buttonitem.Width = 35;
 				Buttonitem.Background = new SolidColorBrush(Colors.White);
 				Thickness m = Buttonitem.Margin;
 				m.Left = newMargin;
 				newMargin += 36;
-				Buttonitem.Margin = m;
+				Buttonitem.Margin = labels[cnt].Margin;
 				Buttonitem.Foreground = new SolidColorBrush(Colors.Black);
 				Buttonitem.FontFamily = new FontFamily("Comic Sans MS");
 				Buttonitem.FontSize = 20;
 				answerLabels.Add(Buttonitem);
+				canvasmiddle.Children.Remove(labels[cnt]);
 				canvasmiddle.Children.Add(Buttonitem);
-
+				cnt++;
 			}
-
 		}
 		public void SetKeyboard()
 		{
 			int margin = 0;
 			int cnt = 0;
-			int[] rndNums = new int[question.Answer.Length];
-			for (int i = 0; i < rndNums.Length; i++)
+			List<int> rndNums = new List<int>();
+			
+			for(int i = 0; i < question.Answer.Length; i++)
 			{
-				rndNums[i] = rnd.Next(0, 40);
-				for (int j = 0; j < rndNums.Length; j++)
-				{
-					while (rndNums[i] == rndNums[j] && i != j)
-					{
-						rndNums[i] = rnd.Next(0, 40);
-					}
-				}
+				rndNums.Add(99);	
 			}
-			for (int i = 0; i <= 40; i++)
+			for (int i = 0; i < rndNums.Count; i++)
+			{
+				rndNums[i] = rnd.Next(1, 41);
+				while(rndNums.Any(s => s == rndNums[i] && rndNums.IndexOf(s) != i))
+				{
+					rndNums[i] = rnd.Next(1, 41);
+				}
+					
+				
+			}
+			for (int i = 1; i <= 40; i++)
 			{
 				Button Buttonitem = new Button();
 				Buttonitem.Content = (char)(rnd.Next(1040, 1071));
-				for (int j = 0; j < rndNums.Length; j++)
+				for (int j = 0; j < rndNums.Count; j++)
 				{
 					if (i == rndNums[j])
 					{
@@ -139,12 +205,12 @@ namespace Viktorina
 				Thickness m = Buttonitem.Margin;
 				m.Left = margin;
 				margin += 36;
-				if (i >= 20)
+				if (i > 20)
 				{
-					if (i == 20)
+					if (i == 21)
 					{
 						m.Left = 0;
-						margin = 0;
+						margin = 36;
 					}
 					m.Top = 37;
 				}
@@ -171,33 +237,34 @@ namespace Viktorina
 				else
 				{
 					MessageBox.Show("Неправильно");
-					foreach (var j in Buttons)
-					{
-						canvas.Children.Remove(j);
-						canvas.Children.Add(j);
-					}
-					foreach (var j in labels)
-					{
-						canvasmiddle.Children.Remove(j);
-					}
-					foreach (var j in answerLabels)
-					{
-						canvasmiddle.Children.Remove(j);
-					}
-					labels = new List<System.Windows.Controls.Label>();
-					answerLabels = new List<System.Windows.Controls.Label>();
-					SetAnswer();
-					closeNums = new int[question.Answer.Length];
-					Answer = new char[question.Answer.Length];
-					newMargin = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
-					cnt = 0;
-
+					/*					foreach (var j in Buttons)
+										{
+											canvas.Children.Remove(j);
+											canvas.Children.Add(j);
+										}
+										foreach (var j in labels)
+										{
+											canvasmiddle.Children.Remove(j);
+										}
+										foreach (var j in answerLabels)
+										{
+											canvasmiddle.Children.Remove(j);
+										}
+										labels = new List<System.Windows.Controls.Label>();
+										answerLabels = new List<System.Windows.Controls.Label>();
+										SetAnswer();
+										closeNums = new int[question.Answer.Length];
+										Answer = new char[question.Answer.Length];
+										newMargin = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
+										cnt = 0;
+					*/
+					SetAllParam();
 					return;
 				}
 			}
 			MessageBox.Show("Правильно");
 			questionList.Remove(question);
-			if(questionList.Count == 0)
+			/*if(questionList.Count == 0)
 			{
 				MessageBox.Show("Вопросы закончились");
 				this.NavigationService.GoBack();
@@ -224,7 +291,9 @@ namespace Viktorina
 			Answer = new char[question.Answer.Length];
 			newMargin = Convert.ToInt32(canvasmiddle.Width / 2 - (question.Answer.Length * 36) / 2);
 			cnt = 0;
-			SetKeyboard();
+			SetKeyboard();*/
+			SetAllParam();
+			questionList.Remove(question);
 		}
 	}
 	
